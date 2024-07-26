8c0d465c757f537c8698b771f5d8c9ec7cca65b3
Dragonte <2592188628@qq.com>
2024-07-26 14:49:16 +0800

    feature:warp列表支持拼音查询，加入gradle-shader，把依赖包打进jar

--- src/main/java/cn/timelessmc/teleport/warp/WarpHandler.java
+++ src/main/java/cn/timelessmc/teleport/warp/WarpHandler.java
@@ -1,13 +1,16 @@
 package cn.timelessmc.teleport.warp;
 
+import com.github.promeg.pinyinhelper.Pinyin;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
+
 
 public class WarpHandler implements TabExecutor {
     private final WarpEntryMap warpEntryMap;
@@ -36,10 +39,27 @@ public class WarpHandler implements TabExecutor {
     }
 
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+       HashMap <String, String> pinYinWarpList = getPinYinWarpList(this.warpEntryMap.keySet());
         return switch (args.length) {
             case 0 -> this.warpEntryMap.keySet().stream().toList();
-            case 1 -> this.warpEntryMap.keySet().stream().filter((s) -> s.startsWith(args[0])).toList();
+//            case 1 -> this.warpEntryMap.keySet().stream().filter((s) -> s.startsWith(args[0])).toList();
+            case 1 -> {
+                String[] argss = pinYinWarpList.keySet().stream().filter((s) -> s.startsWith(args[0])).toArray(String[]::new);
+                for (int i = 0; i < argss.length; i++) {
+                    argss[i] = pinYinWarpList.get(argss[i]);
+                }
+                yield List.of(argss);
+            }
             default -> this.empty;
         };
+    }
+
+    public HashMap<String, String> getPinYinWarpList(Set<String> warpList) {
+        HashMap<String, String> pinYinWarpList = new HashMap<>();
+        for(String warpName:warpList) {
+            String pinyinWarpName= Pinyin.toPinyin(warpName, "");
+            pinYinWarpList.put(pinyinWarpName.toLowerCase(), warpName);
+        }
+        return pinYinWarpList;
     }
 }
